ca6e16a317d99eb627d2a98346d5879f171c82a2
ezinkadoukpe <kadoukpezin@outlook.com>
2023-05-23 23:17:56 -0700

    Going

---
+++ models/base_model.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import uuid
+from datetime import datetime
+
+class BaseModel:
+    """ class BaseModel that defines all common attributes/methods for other classes"""
+    def __init__(self):
+        """Instantition"""
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+
+    def __str__(self):
+        """string out"""
+        class_name = self.__class__.__name__
+        return "[{}] ({}) {}".format(class_name, self.id, self.__dict__)
+
+    def save(self):
+        """updates the public instance attribute updated_at with the current datetime"""
+        self.updated_at = datetime.now()
+
+    def to_dict(self):
+        """returns a dictionary containing all keys/values of __dict__ of the instance"""
+        my_obj = self.__dict__.copy()
+        my_obj['__class__'] = self.__class__.__name__
+        my_obj['created_at'] = self.created_at.isoformat()
+        my_obj['updated_at'] = self.updated_at.isoformat()
+        return my_obj
