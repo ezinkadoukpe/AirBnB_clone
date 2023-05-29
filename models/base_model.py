5195de27f2581f131d1903211739992b0cb6836d
ezinkadoukpe <kadoukpezin@outlook.com>
2023-05-29 04:06:19 -0700

    Done

--- models/base_model.py
+++ models/base_model.py
@@ -19,7 +19,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-            models.storage.new()
+            models.storage.new(self)
 
     def __str__(self):
         """string out"""
