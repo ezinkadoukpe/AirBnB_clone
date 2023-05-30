07810b34128130f1d13aba24e51947d1b011b5eb
ezinkadoukpe <kadoukpezin@outlook.com>
2023-05-29 23:28:38 -0700

    Done

--- console.py
+++ console.py
@@ -14,21 +14,15 @@ class HBNBCommand(cmd.Cmd):
     prompt = "(hbnb) "
 
     def do_quit(self, arg):
-        """
-        Quit command to exit the program
-        """
+        """Quit command to exit the program"""
         return True
 
     def do_EOF(self, arg):
-        """
-        Exit the program on EOF (Ctrl+D)
-        """
+        """Exit the program on EOF (Ctrl+D)"""
         return True
 
     def emptyline(self):
-        """
-        Do nothing when an empty line is entered
-        """
+        """Do nothing when an empty line is entered"""
         pass
 
 
