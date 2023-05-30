8f21355c0562a41bdb5fde798e155e343f68a1c0
ezinkadoukpe <kadoukpezin@outlook.com>
2023-05-29 23:22:38 -0700

    Done

---
+++ console.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """
+        Quit command to exit the program
+        """
+        return True
+
+    def do_EOF(self, arg):
+        """
+        Exit the program on EOF (Ctrl+D)
+        """
+        return True
+
+    def emptyline(self):
+        """
+        Do nothing when an empty line is entered
+        """
+        pass
+
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
+
